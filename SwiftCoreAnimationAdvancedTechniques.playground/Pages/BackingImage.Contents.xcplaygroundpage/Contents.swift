5379152105de8f5a712beaf1c55b9947db1a9849
Danila Goriachkin <dndv279@gmail.com>
2022-08-03 20:33:14 +0300

    Add "Backing Image Contents"

---
+++ SwiftCoreAnimationAdvancedTechniques.playground/Pages/BackingImage.Contents.xcplaygroundpage/Contents.swift
@@ -0,0 +1,30 @@
+/*:
+ # Contents
+ ### Backing Image
+ [⏪ Previous](@previous)    [📄 Contents](Contents)    [Next ⏩](@next)
+ */
+/*:
+ Contents for CGLayer is an object that provides the contents of the layer. Animatable.
+ - Important: If you supply anything other than a CGImage to layer contents, then your layer will be blank (for iOS)
+ */
+//: ## Example
+//: ### Listing2.1 Setting a CGImage as the Layer contents
+import UIKit
+
+final class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load an image
+        let image = UIImage(named: "snowman")!
+        
+        // Add it to our view's layer
+        self.view?.layer.contents = image.cgImage
+    }
+}
+
+present(ViewController())
+/*:
+ - Note: Contents works with CGImage
+ */
--- SwiftCoreAnimationAdvancedTechniques.playground/Pages/Contents.xcplaygroundpage/Contents.swift
+++ SwiftCoreAnimationAdvancedTechniques.playground/Pages/Contents.xcplaygroundpage/Contents.swift
@@ -5,4 +5,7 @@
  - [CALayer Tree](CALayerTree) [Listing 1.1]
  - Additional:
     - [CALayer and UIViews](CALayers&UIViews)
+ 
+ - Backing Image:
+    - [Contents](BackingImage.Contents) [Listing 2.1]
  */
