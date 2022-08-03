ad0b90d5910f963b0fff58b5db38638bd6662396
Danila Goriachkin <dndv279@gmail.com>
2022-08-03 22:51:52 +0300

    Add "Backing Image ContentsGravity"

---
+++ SwiftCoreAnimationAdvancedTechniques.playground/Pages/BackingImage.contentsGravity.xcplaygroundpage/Contents.swift
@@ -0,0 +1,33 @@
+/*:
+ # contentsGravity
+ ### Backing Image
+ [⏪ Previous](@previous)    [📄 Contents](Contents)    [Next ⏩](@next)
+ */
+/*:
+ ContentsGravity is determine how content should be aligned within the layer bounds.
+ - Note: ```view?.layer.contentsGravity = .resizeAspect``` analogously ```view.contentMode = .scaleAspectFit```
+ */
+//: ## Example
+//: ### Listing2.1* Using contentsGravity to change image alignment
+import UIKit
+
+final class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load an image
+        let image = UIImage(named: "snowman")!
+        
+        // Add it to our view's layer
+        self.view?.layer.contents = image.cgImage
+        
+        // Scale image to fit the layer bounds without distorting its aspect ratio
+        self.view?.layer.contentsGravity = .resizeAspect
+    }
+}
+
+present(ViewController())
+/*:
+ - Note: Our snowman less fat than at the [previous](@previous) example
+ */
